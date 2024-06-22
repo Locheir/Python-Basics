795b9040155e82e5823c8b1b2c0f2a99d44b706c
MOHIT GUPTA <mohitguptadp7@gmail.com>
2024-06-22 15:10:06 +0530

    second commit

---
+++ 5-Modules and Packages/test.py
@@ -0,0 +1,13 @@
+from package.math1 import *
+from package.sub_package.math2 import *
+
+print(addition(2,3))
+print(subtraction(7,1))
+
+print(multiplication(4,3))
+
+# To run this
+# Go to Terminal
+# This file location
+# Type : python test.py
+
---
+++ README.md
@@ -0,0 +1,3 @@
+git remote add origin https://github.com/Locheir/Python-Basics.git
+git branch -M main
+git push -u origin main
